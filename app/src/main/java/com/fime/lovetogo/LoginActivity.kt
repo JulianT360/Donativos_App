bb2b21205d070e0c41e0e435cbf28fe0ddc77b55
Julian Tovar <julian.tovar360@gmail.com>
2021-10-05 20:58:17 -0500

    Se reconfiguran strings y pantallas

--- app/src/main/java/com/fime/lovetogo/LoginActivity.kt
+++ app/src/main/java/com/fime/lovetogo/LoginActivity.kt
@@ -1,11 +1,29 @@
 package com.fime.lovetogo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Redirige a la pantalla de registro
+        val btnRegister = findViewById<TextView>(R.id.btn_register)
+        btnRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+
 }
